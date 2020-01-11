34ea01148ef070644ab095a1831b818f619c09bd
Jonathan T Dean <jonathantomdean@gmail.com>
2020-01-11 03:45:42 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,33 @@
+a = 8
+b = 4
+
+function add(a, b){
+  return (a + b)
+}
+
+function subtract(a, b){
+  return (a - b)
+}
+
+function multiply(a, b){
+  return (a * b)
+}
+
+function divide(a, b){
+  return (a / b)
+}
+
+function inc(n){
+ var n = 287 
+ return n += n
+}
+
+function dec(n){
+  var n = 229
+  return n += n
+}
+
+function makeInt(n){
+  return parseInt(10, "boogle")
+
+}
